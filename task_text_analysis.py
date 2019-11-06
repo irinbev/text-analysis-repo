b5061d27e3a964e1a25b8080ef49d4d9441ba744
Irina Bevz <irina.bevz7@gmail.com>
2019-11-06 15:57:12 +0100

    comments to the code added added project file changes done

--- task_text_analysis.py
+++ task_text_analysis.py
@@ -32,7 +32,7 @@ in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
          ]
 
-library = {
+credentials = {
     'bob': '123',
     'ann': 'pass123',
     'mike': 'password123',
@@ -49,72 +49,72 @@ print('Welcome to the Text Analyser')
 
 # 2 and 3.enter credentials and check for correctness
 
-if library.get(username) != password:
+if credentials.get(username) != password:
     print('Access denied!')
-elif library.get(username) == password:
+    exit()
+elif credentials.get(username) == password:
     print('Access allowed')
 
 print('USERNAME: ', username)
-print('PASSWORD: ', password)
+
 print('-' * 40)
 
 # 4.choose from 3 texts
 
 print('We have 3 texts to be analyzed.')
 
-text = int(input('Enter a number btw. 1 and 3 to select: '))
-number = TEXTS[text - 1]
+selection = input('Enter a number btw. 1 and 3 to select: ')
+number = 1
 
-if text == 1:
-    print(number)
-elif text == 2:
-    print(number)
-elif text == 3:
-    print(number)
+if int(selection) in (1, 2, 3) and (selection.isdigit()):
+    print('Input is correct')
+    text = TEXTS[int(selection) - number]
+    print(text)
 else:
-    print('The number entered is outside the given range')
+    print('Please enter a number only from 1 to 3')
+    exit()
 
 print('-' * 40)
 
 # 5. calculate statistics
 
-total_words = len(number.split())
+formatted = text.split()
+
+total_words = len(formatted)
 print('There are ', total_words, 'words in the selected text.')
 
-capital = sum(map(str.istitle, number.split()))
+capital = sum(map(str.istitle, formatted))
 print('There are ', capital, 'title case words')
 
-uppercase = sum(map(str.isupper, number.split()))
+uppercase = sum(map(str.isupper, formatted))
 print('There are ', uppercase, 'upper case words')
 
-lowercase = sum(map(str.islower, number.split()))
+lowercase = sum(map(str.islower, formatted))
 print('There are ', lowercase, 'lower case words')
 
-numeric = sum(map(str.isnumeric, number.split()))
+numeric = sum(map(str.isnumeric, formatted))
 print('There are ', numeric, 'numeric case words')
 
 print('-' * 40)
 
 # 6. calculate a chat: how many words of what length are presented in the text
 
-formatted = number.split()
-
 words_count = {}
 
 while formatted:
-    words = len(formatted.pop())
-    words_count[words] = words_count.get(words, 0) + 1
+    words_lenght = len(formatted.pop())
+    words_count[words_lenght] = words_count.get(words_lenght, 0) + 1
 print(sorted(words_count.items()))
 
 while words_count:
     info = words_count.popitem()
-    print(str(info[0]) + ' char has length of ' + str(info[1]) + ' word/s')
+    print(str(info[0]) + ' ' + str((info[1]) * '*') + ' ' + str(info[1]))
 
 print('-' * 40)
 
 # 7. calculate the sum of all the numeric "words" in the given text
 
-numeric_sum = sum(map(int, filter(str.isnumeric, number.split())))
+numeric_sum = sum(map(int, filter(str.isnumeric, text.split())))
 print('If we summed all the numbers in this text we would get: ', numeric_sum)
 
 print('-' * 40)
